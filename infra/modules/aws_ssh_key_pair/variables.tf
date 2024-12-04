6461b43d43a3d8a08737bd448323ae0444c1894a
Peilun Huang <82012242+thepeilunhuang@users.noreply.github.com>
2024-12-03 17:07:32 -0800

    Delete infra directory

--- infra/modules/aws_ssh_key_pair/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "key_name" {
-  description = "Name used to for both AWS key pair and local private key file"
-  default     = "ssh_key_pair"
-}
--- infra/modules/terraform_security_group/outputs.tf
+++
@@ -1,5 +0,0 @@
-
-output "sg_1_id" {
-  value = aws_security_group.sg_self.id
-}
-
